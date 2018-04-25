08e0a8e0b88c5aa0cdc3695fb82101d3f5778831
Linh Phan <linhphanhust@gmail.com>
2018-04-25 10:42:40 +0700

    design another view

--- Front-end/js/index.js
+++ Front-end/js/index.js
@@ -1,24 +1,41 @@
 
 $(document).ready(function () {
-  $("#random").on("click", function () {
-    window.open("https://en.wikipedia.org/wiki/Special:Random");
-  });
-  $("#btn-search").on("click", function () {
-    form_search = $("#form-search").val();
+
+
+  $("#form-search ").submit(function (e) {
+    
+    e.preventDefault();
     $(".content").empty();
+    form_search = $("#input-search").val();
+    var url = 'http://localhost:8983/solr/IT4853/select?hl.fl=content&hl=on&q=content:"' + form_search + '"&wt=json&json.wrf=callback'; 
+
     $.ajax({
-      url: 'http://localhost:8983/solr/IT4853/select?&q=content:"' + form_search +'"&wt=json&json.wrf=callback',
+      url: url,
       crossDomain: true,
       dataType: 'jsonp',
       jsonpCallback: 'callback'
-      // dataType: 'jsonp'
     }).done(function (data) {
-      // console.log(data)
-      $.each(data.response.docs, function (a, v) {
+      console.log(data)
+      hightlight = data.highlighting ;
+      // console.log(hightlight)
+
+      $.each(data.response.docs, function (a,v) {
         // console.log(v);
-        var html = '<a target=_blank href="' + v.url[0] + '"><div><span>' + v.title[0] + '</span></a><p>' + v.content[0] + '</p></div>';
+        id = v.id
+        content = v.content[0].substr(0,120) + "..." ;
+        if(hightlight[id].content[0] != "") {
+          content += hightlight[id].content[0] + "..." ;
+        }
+        // content += "..." + v.content[0].substr(-60) ;
+        var html = '<a target=_blank href="' + v.url[0] + '"><div><span>' + v.title[0] + '</span></a><p>' + content + '</p></div>';
+        
+        html = html.replace(/<em>/g,"<b>");
+        html = html.replace(/<\/em>/g,"</b>");
         $(".content").append(html);
       });
     });
+
   });
+
+
 });
